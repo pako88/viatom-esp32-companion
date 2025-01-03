8fbfcd62e24998bcacb7159aba8d90a49e39774d
Pascal Koenig <mail@koenig-pascal.de>
2025-01-03 21:52:16 +0100

    restart ESP if no wifi connection is possible

--- lib/wifi_helper/wifi_helper.cpp
+++ lib/wifi_helper/wifi_helper.cpp
@@ -8,22 +8,18 @@ bool is_wifi_connected() {
 }
 
 void connect_wifi() {
-    bool connected = false;
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-    while(!connected) {
-        WiFi.begin(WIFI_SSID, WIFI_PASS);
+    const int limit = 20;
+    int i = 0;
 
-        const int limit = 20;
-        int i = 0;
-
-        while (WiFi.status() != WL_CONNECTED && i <= limit) {
-            delay(500);
-            Serial.print(".");
-            i++;
-        }
-        if (is_wifi_connected) {
-            connected = true;
-        }
+    while (WiFi.status() != WL_CONNECTED && i <= limit) {
+        delay(500);
+        Serial.print(".");
+        i++;
+    }
+    if (!is_wifi_connected) {
+        ESP.restart();
     }
 
     Serial.print("WiFi connected\tIP address: ");
